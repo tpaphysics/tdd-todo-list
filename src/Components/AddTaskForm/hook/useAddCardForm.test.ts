ee98c3d515b66f2b1418602ab734f16ed13e550e
tpaphysics <physics.thiago@gmail.com>
2022-10-04 15:45:32 -0300

    test: should be task value equal "TEST"

--- src/Components/AddTaskForm/hook/useAddCardForm.test.ts
+++ src/Components/AddTaskForm/hook/useAddCardForm.test.ts
@@ -4,8 +4,15 @@ import { act, renderHook } from '@testing-library/react';
 import { useAddCardForm } from './useAddCardForm';
 
 describe('useCount hook', () => {
-  it('Should be inital value task equal ""', () => {
+  it('Should be inital task value equal ""', () => {
     const { result } = renderHook(() => useAddCardForm());
     expect(result.current.task).toBe('');
+  });
+  it('Should be task value equal "TEST"', () => {
+    const { result } = renderHook(() => useAddCardForm());
+    act(() => {
+      result.current.setTask('TEST');
+    });
+    expect(result.current.task).toBe('TEST');
   });
 });
