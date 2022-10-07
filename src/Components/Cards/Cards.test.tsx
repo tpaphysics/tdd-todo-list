44cd12e31a292f7544de9a76490b9ccd381b3880
tpaphysics <physics.thiago@gmail.com>
2022-10-07 15:07:37 -0300

    test: should be in the document a lot of tasks

--- src/Components/Cards/Cards.test.tsx
+++ src/Components/Cards/Cards.test.tsx
@@ -1,7 +1,18 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { getByText, render } from '@testing-library/react';
 import { describe } from 'vitest';
 import ListProvider from '../../global/useList/provider/ListProvider';
 import Cards from '../Cards/index';
 
-describe('AddCardFormvcomponent', () => {});
+describe('Cards component', () => {
+  it('Should be in the Cards a lot of tasks', () => {
+    const { getByText } = render(
+      <ListProvider>
+        <Cards />
+      </ListProvider>,
+    );
+    expect(getByText(/tarefa 1/i)).toBeInTheDocument();
+    expect(getByText(/tarefa 2/i)).toBeInTheDocument();
+  });
+});
