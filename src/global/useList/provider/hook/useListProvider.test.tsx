d6ea2295d442862ae017e3a3e1a112b2781c7756
tpaphysics <physics.thiago@gmail.com>
2022-10-07 15:30:02 -0300

    test: should be newCardMock equal final

--- src/global/useList/provider/hook/useListProvider.test.tsx
+++ src/global/useList/provider/hook/useListProvider.test.tsx
@@ -1,11 +1,12 @@
 import '@testing-library/jest-dom';
 import { describe } from 'vitest';
-import { act, render, renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import cards from '../../../../data/cards';
 import { useListProvider } from './useListProvider';
+import { CardData } from '../../../../data/CardData.interface';
 
 describe('useListProvider hook', () => {
-  it('Should be cards equal cardsMock ', () => {
+  it('Should be cards equal cardsMock', () => {
     const cardsMock = cards;
     const { result } = renderHook(() => useListProvider(cardsMock));
 
@@ -13,5 +14,19 @@ describe('useListProvider hook', () => {
       result.current.cards;
     });
     expect(result.current.cards).toBe(cardsMock);
+  });
+
+  it('Should be newCardMock equal final', () => {
+    const cardsMock = cards;
+    const newCardMock = { id: '12345', completed: false, task: 'New Task' } as CardData;
+
+    const { result } = renderHook(() => useListProvider(cardsMock));
+
+    act(() => {
+      result.current.addCard(newCardMock);
+    });
+    const [final] = result.current.cards.filter((card) => card === newCardMock);
+
+    expect(final).toBe(newCardMock);
   });
 });
